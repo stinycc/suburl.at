7e486a041186b1308abbb2b1fa319fa6a4233bea
stinycc <79641931+stinycc@users.noreply.github.com>
2021-03-21 17:20:00 +0900

    Update master.js

--- resources/master.js
+++ resources/master.js
@@ -124,7 +124,6 @@ function elementFadeIn(elementId, time) {
         id(elementId).style.opacity = 0
     }else{
         id(elementId).style.opacity = id(elementId).offset('opacity')
-        alert('dd')
     }
     if(time){
         animation(elementId, 'fadeIn ' + time/1000 + 's cubic-bezier(0.390, 0.575, 0.565, 1.000) both')
